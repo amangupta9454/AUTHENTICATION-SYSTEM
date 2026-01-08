c9b8a84ae268f4be9e28b08113b8aa3e03ca1525
CODE NOVA <codenova31@gmail.com>
2026-01-08 19:51:40 +0530

    link change

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Navbar from './Pages/Navbar.jsx'
-import Home from './Pages/Home.jsx'
-import Contact from './Pages/Contact.jsx'
 import Register from './Components/Register.jsx'
 import Login from './Components/Login.jsx'
 import Dashboard from './Components/Dashboard.jsx'
@@ -10,10 +7,8 @@ const App = () => {
   return (
     
     <Router>
-      {/* <Navbar /> */}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
